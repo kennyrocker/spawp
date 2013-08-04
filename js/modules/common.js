afa52c9873b739281b8905199345ad5b185cc1bb
Ken Chen <kennyrocker82@gmail.com>
2013-08-04 12:32:35 -0400

    add validate and validate field functioninside common.js

--- js/modules/common.js
+++ js/modules/common.js
@@ -3,36 +3,25 @@
  * K_Common is a class that contains common, reuable functions accessible from anywhere.
  */
 function common(){
+	var self = this;
 	/*reqire fill valiatior*/
 	this.isFilled =function(obj){
-	return obj.str !=='';
+		return obj.str !=='';
 	};
 	/*regular expression validator*/
 	this.isValid =function(obj){
 		var matchBln = obj.matchBln;
 		var str = obj.str;
 		var regStr = new RegExp(obj.regStr);
-		if(str !==''){
-			if(matchBln === false){
-				//console.log('false regStr : '+regStr+' || match : '+regStr.test(str));
-				if(regStr.test(str)){
-				//console.log('0');
-					return false;
-				}else{
-				//	console.log('1');
-					return true;
-				}
-			}else{
-				//console.log('else regStr : '+regStr+' || match : '+regStr.test(str));
-				if(regStr.test(str)){
-				//console.log('0');
-					return true;
-				}else{
-				//console.log('1');
-					return false;
-				}
+		if(str !==''){	// if str is not empty
+			if(matchBln === false){	// if set to NOT match the RegExp
+				var nmBln = regStr.test(str) ? false : true;
+				return nmBln;
+			}else{	// if set to match the RegExp
+				var mBln = regStr.test(str) ? true : false;
+				return mBln;
 			}
-		}else{
+		}else{	// if str is empty
 			return true;
 		}
 	};
@@ -40,11 +29,12 @@ function common(){
 	this.showError=function(obj){
 		var id = obj.id;
 		var message = obj.message;
+		var hex = obj.hex;
 		var cls = obj.cls || 'error';
 			// grab error span innerText
 			var fullErrorStr = $('.'+id+'-'+cls).html();
 			// make input text red
-			$('#'+id).css('color','red');
+			$('#'+id).css('color',hex);
 			// check if message is not within innerText
 			if((fullErrorStr).indexOf(message)!= -1){
 				// if message already exsited, do nothing
@@ -61,10 +51,11 @@ function common(){
 	this.killError=function(obj){
 		var id = obj.id;
 		var message = obj.message;
+		var hex = obj.hex;
 		var cls = obj.cls || 'error';
 			// make reset input text color
 			$('#'+id).focus(function(){
-				$(this).css('color','black');
+				$(this).css('color',hex);
 			});
 			// grab error span innerText
 			var fullErrorStr = $('.'+id+'-'+cls).html();
@@ -77,6 +68,68 @@ function common(){
 			// fade in error span
 			$('.'+id+'-'+cls).fadeIn('slow');
 
+	};
+	/*call multipule validation methods and return a bln*/
+	this.validate=function(obj){
+		// define global validate bln as false
+		var validateBln = false;
+		var fields = obj.fields;
+		// if fields count more than 0
+		if(fields.length > 0){
+			for(var i=0; i<fields.length; i++){
+				// prep each field obj
+				var fieldObj = fields[i];
+					fieldObj.standerHex = obj.standerHex;
+					fieldObj.errorHex = obj.errorHex;
+				// call validate each field
+				validateBln = this.validateField(fieldObj);	
+				console.log('validateBln in validate 1'+validateBln);
+			}
+		}
+		console.log('validateBln in validate 2'+validateBln);
+		return validateBln;
+	};
+	/*validate each field*/
+	this.validateField=function(obj){
+		//console.log(obj);
+		var validateBln = false;
+		var errorNum = 0;
+		var valueStr = $('#'+obj.id).val();
+			//if require field
+			if(obj.requireBln===true){
+				var errorObj = {id:obj.id,hex:obj.standerHex,message:obj.requireMessageStr};
+				if(self.isFilled({str:valueStr})){
+					self.killError(errorObj);
+					//validateBln = true;
+					console.log('vf rf -1');
+					errorNum --;
+				}else{
+					errorObj.hex = obj.errorHex;
+					self.showError(errorObj);
+					//validateBln = false;
+					console.log('vf rf +1');
+					errorNum ++;
+				}
+			}
+			//if no script allow
+			if(obj.noScriptBln===true){
+				var errorObj = {id:obj.id,hex:obj.standerHex,message:'No script allow!!'};
+				if(self.isValid({str:valueStr,matchBln:false,regStr:'<script>'})){
+					self.killError(errorObj);
+					//validateBln = true;
+					console.log('vf ns -1');
+					errorNum --;
+				}else{
+					errorObj.hex = obj.errorHex;
+					self.showError(errorObj);
+					//validateBln = false;
+					console.log('vf ns +1');
+					errorNum ++;
+				}
+			}
+			//if regular expressions
+		console.log('errorNum --> '+errorNum);
+		return validateBln;
 	};
 
 }
--- js/views/admin/K_LoginView.js
+++ js/views/admin/K_LoginView.js
@@ -22,50 +22,76 @@ var K_LoginView = Backbone.View.extend({
 		return this;
 	},
 	loginFnc:function(){
-		var vBln = false;
+		var loginBln = false;
 		//get user name
-		var userNameStr = $('#username-input').val();
+		//var userNameStr = $('#username-input').val();
 		//get pass word
-		var passWordStr = $('#password-input').val();
+		//var passWordStr = $('#password-input').val();
+		// prep valdation object
+		var valiObj = {
+			fields:[
+				{	
+					id:'username-input',
+					requireBln:true,
+					requireMessageStr:'username required',
+					regExp:'[a-Z]',
+					regExpMessageStr:'no number allow',
+					noScriptBln:true
+				},
+				{
+					id:'password-input',
+					requireBln:true,
+					requireMessageStr:'password required',
+					regExp:'[0-9]',
+					regExpMessageStr:'no character allow',
+					noScriptBln:true
+				}
+			],
+			standerHex:'#000',
+			errorHex:'red'
+		};
+		//call validation
+		loginBln = this.validate(valiObj);
+		console.log("FINAL loginBln => "+loginBln);
 		//validation
+		/*
 		if(common.isFilled({str:userNameStr})){
-			common.killError({id:'username-input',message:'username required'});
+			common.killError({id:'username-input',hex:'#000',message:'username required'});
 		}else{
-		 	common.showError({id:'username-input',message:'username required'});
+		 	common.showError({id:'username-input',hex:'red',message:'username required'});
 		}
 		if(common.isFilled({str:passWordStr})){
-			common.killError({id:'password-input',message:'password required'});
+			common.killError({id:'password-input',hex:'#000',message:'password required'});
 		}else{
-			common.showError({id:'password-input',message:'password required'});
+			common.showError({id:'password-input',hex:'red',message:'password required'});
 		}
-		
-		//console.log("view name with script regx "+common.isValid({str:userNameStr,matchBln:false,regStr:'<script>'}));
-		//console.log("view pass with script regx "+common.isValid({str:passWordStr,matchBln:false,regStr:'<script>'}));
-		
+
 		if(common.isValid({str:userNameStr,matchBln:false,regStr:'<script>'})){
-			common.killError({id:'username-input',message:'no script allow'});
+			common.killError({id:'username-input',hex:'#000',message:'no script allow'});
 		}else{
-			common.showError({id:'username-input',message:'no script allow'});
+			common.showError({id:'username-input',hex:'red',message:'no script allow'});
 		}
 		if(common.isValid({str:passWordStr,matchBln:false,regStr:'<script>'})){
-			common.killError({id:'password-input',message:'no script allow'});
+			common.killError({id:'password-input',hex:'#000',message:'no script allow'});
 		}else{
-			common.showError({id:'password-input',message:'no script allow'});
+			common.showError({id:'password-input',hex:'red',message:'no script allow'});
 		}
-		
-		//console.log("view name with char regx "+common.isValid({str:userNameStr,regStr:'[a-z]'}));
-		//console.log("view pass with num regx "+common.isValid({str:passWordStr,regStr:'[0-9]'}));
-		
+
 		if(common.isValid({str:userNameStr,regStr:'[a-z]'})){
-			common.killError({id:'username-input',message:'no number allow'});
+			common.killError({id:'username-input',hex:'#000',message:'no number allow'});
 		}else{
-			common.showError({id:'username-input',message:'no number allow'});
+			common.showError({id:'username-input',hex:'red',message:'no number allow'});
 		}
 		if(common.isValid({str:passWordStr,regStr:'[0-9]'})){
-			common.killError({id:'password-input',message:'no character allow'});
+			common.killError({id:'password-input',hex:'#000',message:'no character allow'});
 		}else{
-			common.showError({id:'password-input',message:'no character allow'});
+			common.showError({id:'password-input',hex:'red',message:'no character allow'});
 		}
-		
+		*/
+	},
+	validate:function(obj){
+		var vBln = common.validate(obj);
+		console.log('validate vBln => '+vBln);
+		return vBln;
 	}
 });
